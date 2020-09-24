78d6c441b35499dabbb4239eadc05eca98f30d9f
Aldon Smith <asmith@smithbros.dev>
2020-09-24 12:34:07 -0400

    Implemented Menu
    Implemented a hamburger style menu

---
+++ duemore-1/MenuView.swift
@@ -0,0 +1,84 @@
+//
+//  MenuView.swift
+//  duemore-1
+//
+//  Created by Aldon Smith on 9/24/20.
+//  Copyright © 2020 Aldon Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack (alignment: .leading) {
+                HStack {
+                    Image(systemName: "tray.and.arrow.down.fill")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 24))
+                    Text("Inbox")
+                        .foregroundColor(.gray)
+                        .font(Font.custom("OpenSans-SemiBold", size: 20))
+                }
+                .padding(.top, 100)
+                HStack {
+                    Image(systemName: "square.and.pencil")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 24))
+                    Text("Tasks")
+                        .foregroundColor(.gray)
+                        .font(Font.custom("OpenSans-SemiBold", size: 20))
+                }
+                .padding(.top, 30)
+                HStack {
+                    Image(systemName: "folder")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 24))
+                    Text("Projects")
+                        .foregroundColor(.gray)
+                        .font(Font.custom("OpenSans-SemiBold", size: 20))
+                }
+                .padding(.top, 30)
+                HStack {
+                    Image(systemName: "calendar")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 24))
+                    Text("Calendar")
+                        .foregroundColor(.gray)
+                        .font(Font.custom("OpenSans-SemiBold", size: 20))
+                }
+                .padding(.top, 30)
+                HStack {
+                    Image(systemName: "line.horizontal.3.decrease.circle")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 24))
+                    Text("Filters")
+                        .foregroundColor(.gray)
+                        .font(Font.custom("OpenSans-SemiBold", size: 20))
+                }
+                .padding(.top, 30)
+                Spacer()
+                HStack {
+                    Image(systemName: "gear")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 24))
+                    Image(systemName: "person.circle")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 24))
+                        .frame(width: geometry.size.width / 2.1, alignment: .trailing)
+                }
+                .padding(.vertical, 30)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
